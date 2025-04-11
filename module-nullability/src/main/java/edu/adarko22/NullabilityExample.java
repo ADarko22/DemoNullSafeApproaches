c5b2ec6a6923511ed91e1843d6b85847d6dd8a05
Angelo Buono <abuono@pictet.com>
2025-04-11 18:04:19 +0200

    Add module-openapi-nullability

--- module-nullability/src/main/java/edu/adarko22/NullabilityExample.java
+++ module-nullability/src/main/java/edu/adarko22/NullabilityExample.java
@@ -4,23 +4,23 @@ import javax.annotation.Nullable;
 
 public class NullabilityExample {
 
-    public void nonNullMethod(String param) {
-        System.out.println(param);
-    }
+  public void nonNullMethod(String param) {
+    System.out.println(param);
+  }
 
-    // Method with @Nullable
-    public void nullableMethod(@Nullable String param) {
-        if (param != null) {
-            System.out.println(param);
-        } else {
-            System.out.println("Null parameter");
-        }
+  // Method with @Nullable
+  public void nullableMethod(@Nullable String param) {
+    if (param != null) {
+      System.out.println(param);
+    } else {
+      System.out.println("Null parameter");
     }
+  }
 
-    public static void main(String[] args) {
-        NullabilityExample example = new NullabilityExample();
-        example.nonNullMethod("Safe");
-        example.nonNullMethod(null);
-        example.nullableMethod(null);
-    }
+  public static void main(String[] args) {
+    NullabilityExample example = new NullabilityExample();
+    example.nonNullMethod("Safe");
+    example.nonNullMethod(null);
+    example.nullableMethod(null);
+  }
 }
--- module-nullability/src/main/java/edu/adarko22/package-info.java
+++ module-nullability/src/main/java/edu/adarko22/package-info.java
@@ -1,3 +1,2 @@
-
 @javax.annotation.ParametersAreNonnullByDefault
 package edu.adarko22;
