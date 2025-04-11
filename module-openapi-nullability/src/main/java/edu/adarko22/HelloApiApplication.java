2458590d806a3465d03045a5ba9439eb35ef3618
Angelo Buono <abuono@pictet.com>
2025-04-11 18:04:19 +0200

    Improvements and clean up

--- module-openapi-nullability/src/main/java/edu/adarko22/HelloApiApplication.java
+++ module-openapi-nullability/src/main/java/edu/adarko22/HelloApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HelloApiApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(HelloApiApplication.class, args);
+  }
 }
--- module-openapi-nullability/src/main/java/edu/adarko22/HelloApiController.java
+++ module-openapi-nullability/src/main/java/edu/adarko22/HelloApiController.java
@@ -2,16 +2,15 @@ package edu.adarko22;
 
 import edu.adarko22.api.HelloApi;
 import edu.adarko22.model.MessageResponse;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class HelloApiController implements HelloApi {
 
-    @Override
-    public ResponseEntity<MessageResponse> getHello(String message) {
-        MessageResponse response = new MessageResponse(null);
-        return ResponseEntity.ok(response);
-    }
+  @Override
+  public ResponseEntity<MessageResponse> getHello(String message) {
+    MessageResponse response = new MessageResponse(null);
+    return ResponseEntity.ok(response);
+  }
 }
